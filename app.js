31648074baa8a4c30ea90e95bb5030e71c1fb07f
manuel peña <manpenap@gmail.com>
2025-01-23 23:33:27 -0300

    Se implementan las funcionalidades básicas a la app

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+console.log('Hola Mundo');
+let amigos = [];
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    if (nuevoAmigo == '') {
+        alert("Por favor, inserte un nombre.");
+        return;
+    } else {
+        amigos.push(nuevoAmigo);
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+        mostrarAmigos();
+        return;
+    }
+    
+}
+
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let index = 0; index < amigos.length; index++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[index];
+        listaAmigos.appendChild(li);   
+    }
+}
+
+function sortearAmigo() {
+
+    if (amigos.length == 0) {
+       alert("No hay amigos en la lista."); 
+       return;
+    } else {
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById('resultado').innerHTML = amigoSorteado;
+        return
+    }
+   
+}
